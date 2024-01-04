232e5fc8d50092bb836660e6f9a4f878d9d2337f
python-jain <155644981+python-jain@users.noreply.github.com>
2024-01-04 19:29:02 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,22 +8,7 @@ st.set_page_config(
 )
 
 st.title("STUPIDITY✨")
-def load_lottiefile(filepath: str):
-    with open(filepath,"r") as f:
-        return json.load(f)
-    
-    lottie_coding = load_lottiefile("multipage/coding.json.json")
-    st_lottie(
-        lottie_coding,
-        speed=1,
-        reverse=False,
-        loop=True,
-        quality="low",
-        renderer="svg",
-        height=None,
-        width=None,
-        key=None,
-    )
+
 
 st.subheader("A Website For My Friends")
 st.sidebar.success("Select a page above.")
